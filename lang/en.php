52a64137e764bb8cc0532eed810d43dc2b3adc02
mariogrip <mario.grip@gmail.com>
2013-05-21 19:47:51 +0200

    More to en.php
    Added serverpage and login page...

--- lang/en.php
+++ lang/en.php
@@ -19,10 +19,24 @@ $l_servers = "Servers";
 $l_all_list_serv = "All Listed Servers";
 $l_information = "Information";
 
-
+// Server page
+$l_server_ip = "Server IP";
+$l_created = "Created";
+$l_owner = "Owner";
+$l_Share = "Share";
 
 // Login page
-$l_login-page
+$l_login_page = "Log in";
+$l_username = "Username";
+$l_password = "Password";
+$l_enter_password = "Enter you password!";
+$l_enter_username = "Enter you Username!";
+$l_or = "or";
+$l_regi = "Register Here";
+$l_error_1 = "Please Login before continuing!";
+$l_error_2 = "Incorrect password, Please try again!";
+$l_error_3 = "That user doesn't exist! Please Register or try again!";
+$l_error_4 = "Please Fill in all fields!";
 
 
 // Add server page
