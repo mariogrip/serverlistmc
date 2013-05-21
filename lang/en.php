7880bcaefcaaef2076cec6592371498c89fb2309
mariogrip <mario.grip@gmail.com>
2013-05-21 19:32:11 +0200

    Start of lang file :)

---
+++ lang/en.php
@@ -0,0 +1,32 @@
+// This file is not finished!
+// Lang file for EN, you can use this as an template...
+
+// Configs
+$l_lang = "English";
+$l_create-accont = "Create accont";
+$l_sign-in = "Sign in";
+$l_players = "Players";
+$l_votes = "votes";
+$l_status = "Status";
+$l_online = "Online";
+$l_offline = "Offline";
+$l_ip = "ip";
+$l_id = "id";
+
+// Home page
+$l_home-page = "Home";
+$l_servers = "Servers";
+$l_all-list-serv = "All Listed Servers";
+$l_information = "Information";
+
+
+
+// Login page
+$l_login-page
+
+
+// Add server page
+
+
+
+//Manage server page
