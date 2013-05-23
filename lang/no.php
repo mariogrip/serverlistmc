14a1d731f8aac77a0fe8628963fab1475a546e6e
mariogrip <mario.grip@gmail.com>
2013-05-23 20:25:08 +0200

    Adding no.php (norwegian)

---
+++ lang/no.php
@@ -0,0 +1,57 @@
+// This file is not finished! (Denne filen er ikke ferdig)
+// Norwegin lang file (norsk lang file)
+
+// Configs
+$l_lang = "Norsk";
+$l_create_accont = "Lag konto";
+$l_sign_in = "Logg inn";
+$l_players = "Spillere";
+$l_votes = "poeng";
+$l_status = "Status";
+$l_online = "Online";
+$l_offline = "Offline";
+$l_ip = "ip";
+$l_id = "id";
+
+// Home page
+$l_home_page = "Hjem";
+$l_servers = "Serverene";
+$l_all_list_serv = "Alle listed serverer";
+$l_information = "Infomasjon";
+
+// Server page
+$l_server_ip = "Server IP";
+$l_created = "Laget";
+$l_owner = "Eier";
+$l_Share = "Dele";
+$l_banner = "Banner";
+$l_BBcode = "BBCode til din forum signature";
+$l_HTML = "HTML til din nettside";
+$l_vote = "Stemme!";
+
+// Login page
+$l_login_page = "Logg inn";
+$l_username = "Brukernavn";
+$l_password = "Passord";
+$l_enter_password = "Bruk ditt passord!";
+$l_enter_username = "Bruk ditt Brukernavn!";
+$l_or = "eller";
+$l_regi = "Register deg her";
+$l_error_1 = "Du må logge in før du forsetter!";
+$l_error_2 = "Feil passord prøv igjen!";
+$l_error_3 = "Den brukenen finnes ikke! Vengligs Registrer eller prøv igjen!";
+$l_error_4 = "Vengligs fyll ut alle felt!";
+
+
+// Add server page
+$l_add_your_server = "Legg til din Minecraft server her";
+$l_name = "Navn";
+$l_ip_host = "IP/Host";
+$l_type = "Type";
+$l_websiteurl = "Nettside URL";
+$l_des = "Beskrivese";
+$l_submit = "Send";
+$l_des_name = "";
+
+
+//Manage server page
